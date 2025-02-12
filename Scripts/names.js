86d1c724d990a75f084c7142aa44015489687510
VALENTINE246 <somtoifeduba2@gmail.com>
2025-02-12 16:55:36 +0100

    Add files via upload

---
+++ Scripts/names.js
@@ -0,0 +1,8 @@
+//  Names of students------
+  const name1 = document.getElementById('name1')
+  const status1 = document.getElementById('status1')
+  name1.innerHTML = ''
+  status1.innerHTML = ''
+
+
+  
