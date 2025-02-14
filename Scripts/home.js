902e8e7e0071635b3da4cac82facdc8884259a69
VALENTINE246 <somtoifeduba2@gmail.com>
2025-02-14 13:43:49 +0100

    Add files via upload

---
+++ Scripts/home.js
@@ -0,0 +1,6 @@
+// const mediaQuery = window.matchMedia("(max-width: 650px)")
+
+// if(mediaQuery.matches) {
+//   const welcomeText = document.querySelector(".w-t");
+//   welcomeText.textContent = "Welcome to Cyber Security Department Unizik"
+// }
